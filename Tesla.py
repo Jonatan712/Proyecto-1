5ad9d2fa0e160daf4cc8ca7fa8b1992bff995d1b
Jonatan712 <70024294+Jonatan712@users.noreply.github.com>
2020-09-20 00:03:53 -0500

    Update Tesla.py

--- Tesla.py
+++ Tesla.py
@@ -1,67 +1,93 @@
-aceleracion = input("Inserte aceleracion deseada de 0 - 100 km: ")
-costo = input("Inserte costo en pesos MXN: ")
-Autonomia = input("Autonomia deseada en km: ")
-Asientos = input("Numero de Asientos: ")
+aceleracion = float(input("Inserte aceleracion deseada de 0 - 100 km: "))
+costo = float(input("Inserte costo en pesos MXN: "))
+autonomia = float(input("Autonomia deseada en km: "))
+asientos = float(input("Numero de Asientos: "))
 
 roadster = 0
 models = 0
 modelx = 0
 cybertruck = 0
 
-if( aceleracion > 0 and aceleracion < 2.1):
+if( aceleracion >= 0 and aceleracion <= 2.1):
     roadster = roadster + 1
     
-elif( aceleracion > 2.1 and aceleracion < 2.5):
+elif( aceleracion >= 2.2 and aceleracion <= 2.5):
     models = models + 1
     
-elif( aceleracion > 2.5 and aceleracion < 2.8):
+elif( aceleracion >= 2.6 and aceleracion <= 2.8):
     modelx = modelx + 1
     
-elif( aceleracion > 2.8 and aceleracion < 6.5):
+elif( aceleracion >= 2.9 and aceleracion <= 6.5):
     cybertruck = cybertruck + 1
     
-if( costo > 0 and costo < 875000):
+if( costo >= 0 and costo <= 875000):
     cybertruck = cybertruck + 1
     
-elif( costo > 875000 and costo < 1500000):
+elif( costo >= 875001 and costo <= 1500000):
     models = models + 1
     
-elif( costo > 1500000 and costo < 2100000):
+elif( costo >= 1500001 and costo <= 2100000):
     modelx = modelx + 1
     
-elif( costo > 2100000 and costo < 3840000):
+elif( costo >= 2100001 and costo <= 3840000):
     roadster = roadster + 1
     
-if( autonomia > 0 and autonomia < 400):
+if( autonomia >= 0 and autonomia <= 400):
     cybertruck = cybertruck + 1
     
-elif( autonomia > 400 and autonomia < 490):
+elif( autonomia >= 401 and autonomia <= 490):
     modelx = modelx + 1
     
-elif( autonomia > 490 and autonomia < 560):
+elif( autonomia >= 491 and autonomia <= 560):
     models = models + 1
     
-elif( autonomia > 560 and autonomia <1000):
+elif( autonomia >= 561 and autonomia <= 1000):
     roadster = roadster + 1
     
-if( asientos > 0 and asientos < 3):
+if( asientos >= 0 and asientos <= 4):
     roadster = roadster + 1
     
-elif( asientos > 3 and asientos < 5):
+elif( asientos > 4 and asientos < 6):
     cybertruck = cybertruck + 1
     models = models + 1
 
-elif( aceleracion > 6 and aceleracion < 7):
+elif( asientos >= 6 and asientos <= 7):
     modelx = modelx + 1
     
 
 
     
 if (roadster > models and roadster > modelx and roadster > cybertruck):
-    print("roadster")
-if (models > roadster and models > modelx and models > cybertruck):
-    print("models")
-if (modelx > models and modelx > roadster and modelx > cybertruck):
-    print("modelx")
-if (cybertruck > models and cybertruck > modelx and cybertruck > roadster):
-    print("cybertruck")
+    print("El mejor modelo es = roadster")
+elif (models > roadster and models > modelx and models > cybertruck):
+    print("El mejor modelo es = models")
+elif (modelx > models and modelx > roadster and modelx > cybertruck):
+    print("El mejor modelo es = modelx")
+elif (cybertruck > models and cybertruck > modelx and cybertruck > roadster):
+    print("El mejor modelo es = cybertruck")
+
+    
+if (roadster == 2 ):
+    print("Como segunda opcion seria = roadster")
+    
+if (models == 2):
+    print("Como segunda opcion seria = models")
+    
+if (modelx == 2):
+    print("Como segunda opcion seria = modelx")
+    
+if (cybertruck == 2):
+    print("Como segunda opcion seria = cybertruck")
+    
+elif (models == 1 and roadster == 1 and modelx == 1 and cybertruck == 1):
+    print("Error, intente ser más especifico en los datos")
+    
+
+'''    
+print(modelx)
+print(cybertruck)
+print(models)
+print(roadster)
+'''
+
+    
